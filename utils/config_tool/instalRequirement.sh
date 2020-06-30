257bcf0b34d6729f25a8a7515a91082cba22ac44
杨志勇 <190716@lifeat.cn>
2020-06-30 16:25:33 +0800

    修改获取依赖的脚本，指定阿里源

--- utils/config_tool/instalRequirement.sh
+++ utils/config_tool/instalRequirement.sh
@@ -1,1 +1,1 @@
-pip install -r requirements.txt
+pip install -r requirements.txt -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
---
+++ utils/config_tool/installRequirementPip3.sh
@@ -0,0 +1,1 @@
+pip3 install -r requirements.txt -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
