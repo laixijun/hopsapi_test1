68dd0db821fdc6f0fd28163801fff2f987a5371b
杨志勇 <190716@lifeat.cn>
2020-10-23 17:17:59 +0800

    appium流程测试

--- pyapi/scrapyApi/appium_app/broker_spider.py
+++ pyapi/scrapyApi/appium_app/broker_spider.py
@@ -1,6 +1,7 @@
 # @Time    : 7/21/2020 6:19 PM
 # @Author  : Yang Xiaobai
 # @Email   : yangzhiyongtest@163.com
+import os
 from time import sleep
 
 from pyapi.scrapyApi.appium_app.get_driver import GetDriver
@@ -15,6 +16,7 @@ class SpiderDemo:
     #启动APP
     def startApp(self):
         driver=self.driver
+        driver.implicitly_wait(10)
         el1 = driver.find_element_by_id("com.android.packageinstaller:id/permission_allow_button")
         el1.click()
         el2 = driver.find_element_by_id("com.android.packageinstaller:id/permission_allow_button")
@@ -28,6 +30,10 @@ class SpiderDemo:
         # driver = self.driver
         el4 = driver.find_element_by_id("com.easylife.house.broker.test:id/tabMine")
         el4.click()
+        elementExist = ToolsAppium().is_element_exist(driver=driver, element='常驻城市')
+        if elementExist:
+            el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/sure")
+            el1.click()
         el5 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvlogin")
         el5.click()
         sleep(5)
@@ -35,6 +41,12 @@ class SpiderDemo:
         el6.send_keys(phone)
         el7 = driver.find_element_by_id("com.easylife.house.broker.test:id/btnLogin")
         el7.click()
+        sleep(5)
+        elementExist=ToolsAppium().is_element_exist(driver=driver,element='常驻城市')
+        if elementExist:
+            el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/sure")
+            el1.click()
+        sleep(3)
         return driver
     
     # 我的回到首页
@@ -155,25 +167,32 @@ class SpiderDemo:
         pass
     
     # 企业认证
-    def companyAuthencition(self,driver):
-        driver = self.companyIdentify(driver)
+    def companyAuthencition(self,driver,xinTax):
+        driver = self.companyIdentify(driver,xinTax)
         driver = self.bankAccount(driver)
         driver = self.receiveFund(driver)
+        driver = self.administratorEntry(driver)
+        return driver
+        
     
     # 管理员信息录入
     def administratorEntry(self,driver):
         el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/imgIDCardFront")
         el1.click()
+        sleep(3)
         el2 = driver.find_element_by_xpath(
             "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout[1]/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[10]/android.view.View")
         el2.click()
+        sleep(3)
         el3 = driver.find_element_by_id("com.easylife.house.broker.test:id/button_apply")
         el3.click()
         el4 = driver.find_element_by_id("com.easylife.house.broker.test:id/imgIDCardSide")
         el4.click()
+        sleep(3)
         el5 = driver.find_element_by_xpath(
             "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout[1]/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[12]/android.view.View")
         el5.click()
+        sleep(3)
         el6 = driver.find_element_by_id("com.easylife.house.broker.test:id/button_apply")
         el6.click()
         el7 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvOwnerName")
@@ -183,6 +202,7 @@ class SpiderDemo:
         # 滑动界面
         ta = ToolsAppium()
         driver = ta.swipeUp(driver)
+        sleep(3)
         el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvOwnerIDCardValidDate")
         el1.click()
         # 证件有效期
@@ -193,13 +213,16 @@ class SpiderDemo:
         el2.send_keys("15700000006@163.com")
         el3 = driver.find_element_by_id("com.easylife.house.broker:id/imgAttorney")
         el3.click()
+        sleep(3)
         el4 = driver.find_element_by_xpath(
             "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout[1]/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[9]/android.view.View")
         el4.click()
+        sleep(3)
         el5 = driver.find_element_by_id("com.easylife.house.broker:id/button_apply")
         el5.click()
         el6 = driver.find_element_by_id("com.easylife.house.broker:id/tvSubmit")
         el6.click()
+        sleep(3)
         return driver
 
     # 收款金额验证
@@ -211,7 +234,7 @@ class SpiderDemo:
         sleep(3)
         return driver
     # 企业认证企业信息录入
-    def companyIdentify(self,driver):
+    def companyIdentify(self,driver,xinTax='911101085636363795'):
         el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvAuthState")
         el1.click()
         sleep(3)
@@ -235,7 +258,7 @@ class SpiderDemo:
         el7 = driver.find_element_by_id("com.easylife.house.broker.test:id/edIDCardNum")
         el7.clear()
         sleep(3)
-        el7.send_keys("911101085636363795")
+        el7.send_keys(xinTax)
         el8 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvSubmit")
         el8.click()
         sleep(3)
@@ -252,7 +275,7 @@ class SpiderDemo:
         el12 = driver.find_element_by_id("com.easylife.house.broker.test:id/edIDCardNum")
         el12.clear()
         sleep(3)
-        el12.send_keys(" 911101085636363795")
+        el12.send_keys(xinTax)
         # 滑动界面
         ta = ToolsAppium()
         driver=ta.swipeUp(driver)
@@ -273,21 +296,27 @@ class SpiderDemo:
         el1.clear()
         el1.send_keys("计算机技术")
         # 法人身份证上传
+        driver = ta.swipeUp(driver)
         el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/imgIDCardFront")
         el1.click()
+        sleep(3)
         el2 = driver.find_element_by_xpath(
             "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout[1]/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[6]/android.view.View")
         el2.click()
+        sleep(3)
         el3 = driver.find_element_by_id("com.easylife.house.broker.test:id/button_apply")
         el3.click()
+        sleep(3)
         el4 = driver.find_element_by_id("com.easylife.house.broker.test:id/imgIDCardSide")
         el4.click()
+        sleep(3)
         el5 = driver.find_element_by_xpath(
             "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout[1]/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[9]/android.view.View")
         el5.click()
+        sleep(3)
         el6 = driver.find_element_by_id("com.easylife.house.broker.test:id/button_apply")
         el6.click()
-        driver = ta.swipeUp(driver)
+        sleep(3)
         el2 = driver.find_element_by_id("com.easylife.house.broker.test:id/edOwnerName")
         el2.clear()
         el2.send_keys("薛业乔")
@@ -298,31 +327,60 @@ class SpiderDemo:
         ta.legalSwipeUp(driver)
         el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvSubmit")
         el1.click()
+        sleep(3)
         return driver
         
     # 银行账号录入
     def bankAccount(self,driver):
         el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/imgAddBankCard")
         el1.click()
+        sleep(3)
         el2 = driver.find_element_by_xpath(
             "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout[1]/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[6]/android.view.View")
         el2.click()
+        sleep(3)
         el3 = driver.find_element_by_id("com.easylife.house.broker.test:id/button_apply")
         el3.click()
+        sleep(3)
         el4 = driver.find_element_by_id("com.easylife.house.broker.test:id/edBankCardOwnerName")
         el4.send_keys("工商银行")
+        sleep(3)
         el5 = driver.find_element_by_id("com.easylife.house.broker.test:id/edBankCardNum")
         el5.send_keys("12323234")
+        sleep(1)
         # 滑动界面
         ta = ToolsAppium()
         driver = ta.swipeUp(driver)
         driver = ta.bankChoose(driver)
         driver = ta.bankBranchChoose(driver)
         driver = ta.areaChoose(driver)
+        sleep(3)
         return driver
-        
-        
-        
+    
+    # 认证中
+    def rezhen(self,driver):
+        sleep(3)
+        el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/rl_company_auth")
+        el1.click()
+        sleep(3)
+        el2 = driver.find_element_by_id("com.easylife.house.broker.test:id/btnApply")
+        el2.click()
+        sleep(3)
+        # 滑动界面
+        ta = ToolsAppium()
+        driver = ta.swipeUp(driver)
+        driver = ta.bankBranchChoose(driver)
+        driver = ta.bankChoose(driver)
+        # el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvBankCardOpenBankName")
+        # el1.click()
+        # el2 = driver.find_element_by_id("com.easylife.house.broker.test:id/edSearch")
+        # el2.click()
+        # el3 = driver.find_element_by_id("com.easylife.house.broker.test:id/edSearch")
+        # el3.click()
+        # el3.send_keys("工")
+        # # ToolsAppium().keyCode(type='Sougou')
+        # sleep(3)
+        # driver.keyevent(66)
 
     #点击始终允许
     def always(self):
@@ -347,7 +405,6 @@ class SpiderDemo:
         xml2 = self.driver.find_element_by_css_selector("android.widget.EditText")
         print(xml2)
     
-
 class ToolsAppium:
     def __init__(self):
         pass
@@ -364,6 +421,14 @@ class ToolsAppium:
         def swipSize(self):
             return driver.get_window_size()
     '''
+    # 判断元素是否存在
+    def is_element_exist(self, driver,element):
+        source = driver.page_source
+        print(source)
+        if element in source:
+            return True
+        else:
+            return False
     
     # 选择开户地区
     def areaChoose(self,driver):
@@ -380,34 +445,46 @@ class ToolsAppium:
 
     # 开户行选择
     def bankChoose(self,driver):
-        el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvBankCardOpenBankName")
-        el1.click()
+        el2 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvBankCardOpenBankSubName")
+        el2.click()
+        sleep(3)
         el3 = driver.find_element_by_id("com.easylife.house.broker.test:id/edSearch")
         el3.click()
+        sleep(3)
         el4 = driver.find_element_by_id("com.easylife.house.broker.test:id/edSearch")
         el4.send_keys("工")
-        driver.keyevent('66')
+        sleep(3)
+        # self.keyCode(type='Sougou')
+        driver.keyevent(66)
+        driver.hide_keyboard()
         el5 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvName")
         el5.click()
+        sleep(3)
+        return driver
         
     # 开户行支行选择
     def bankBranchChoose(self,driver):
-        el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvBankCardOpenBankSubName")
+        el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvBankCardOpenBankName")
         el1.click()
+        sleep(3)
+        el2 = driver.find_element_by_id("com.easylife.house.broker.test:id/edSearch")
+        el2.click()
+        sleep(3)
         el3 = driver.find_element_by_id("com.easylife.house.broker.test:id/edSearch")
-        el3.click()
-        el4 = driver.find_element_by_id("com.easylife.house.broker.test:id/edSearch")
-        el4.send_keys("工")
-        driver.keyevent('66')
-        el1 = driver.find_element_by_xpath(
-            "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[7]/android.widget.TextView")
+        el3.send_keys("工")
+        # self.keyCode(type='Sougou')
+        driver.keyevent(66)
+        driver.hide_keyboard()
+        el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvName")
         el1.click()
+        sleep(3)
         return driver
     
     # 企业规模（7分）
     def scaleSwipeUp(self,driver):
         el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvCompanyScale")
         el1.click()
+        sleep(3)
         eleScale = driver.find_element_by_id("com.easylife.house.broker.test:id/wheeView_center")
         driver = self.swipeUp1(driver, ele=eleScale, t=1000, n=1)
         el3 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvok")
@@ -432,6 +509,7 @@ class ToolsAppium:
     def industrySwipeUp(self,driver):
         el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvIndustry")
         el1.click()
+        sleep(3)
         eleIndustry = driver.find_element_by_id("com.easylife.house.broker.test:id/wheeView_center")
         driver = self.swipeUp1(driver, ele=eleIndustry, t=1000, n=1,startRate=0.4,endRate=0.25)
         el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvSubmit")
@@ -470,11 +548,26 @@ class ToolsAppium:
         # 选择城市
         el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvProvince")
         el1.click()
+        sleep(3)
         elePrivince = driver.find_element_by_id("com.easylife.house.broker.test:id/wheelProvince")
         driver = self.swipeUp1(driver, ele=elePrivince, t=1000, n=1,startRate=0.4,endRate=0.25)
         el1 = driver.find_element_by_id("com.easylife.house.broker.test:id/tvOk")
         el1.click()
         return driver
+
+    # 调用输入法解决输入
+    def keyCode(self,type='Sougou'):
+        command2='adb shell ime set com.sohu.inputmethod.sogou/.SogouIME'
+        command3='adb shell ime set io.appium.android.ime/.UnicodeIME'
+        
+        # 列出系统现在所安装的所有输入法 os.system(command0)
+        # 打印系统当前默认的输入法 os.system(command1)
+        # 切换华为输入法为当前输入法 os.system(command2)
+        # 切换appnium输入法为当前输入法 os.system(command3)
+        if type == 'Sougou':
+            os.system(command2)
+        elif type == 'Appium':
+            os.system(command3)
 
     # 向上滑动屏幕
     def swipeUp(self,driver, t=1000, n=1):
@@ -541,16 +634,19 @@ class ToolsAppium:
         wait.until(EC.presence_of_element_located((By.ID, idContent)))
         return driver
 
+
 if __name__ == "__main__":
     sd=SpiderDemo()
     driver=sd.startApp()
-    driver=sd.loginApp(driver,phone='17610775285')
+    driver=sd.loginApp(driver,phone='13025896542')
     sleep(5)
-    driver = sd.companyAuthencition(driver)
-    ta=ToolsAppium()
+    driver= sd.rezhen(driver)
+    # driver = sd.companyAuthencition(driver,xinTax='911000001000013428')
+    # ta=ToolsAppium()
     # ta.swipeUp(driver)
     # ta.swipeUp(driver)
     # ta.swipeDown(driver)
     
 
     # sd.myCommission(driver)
+
